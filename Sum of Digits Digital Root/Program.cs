1386ddc79756bf845d01840731886f4f04da8bbb
newSide123123 <newSide01901@tutamail.com>
2024-07-18 14:16:48 +0300

    Add new feature

---
+++ Sum of Digits Digital Root/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sum_of_Digits_Digital_Root
+{
+    internal class Program
+    {
+        public static int DigitalRoot(long n) => n < 10 ? (int)n : DigitalRoot(n.ToString().Sum(c => c - '0'));
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitalRoot(16));
+        }
+    }
+}
